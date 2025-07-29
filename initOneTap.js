31ea24f7088719b86332b1b22ac8704a8aa8b92f
1v1ad <1v1ad@proton.me>
2025-07-29 15:19:16 +0300

    Add files via upload

--- initOneTap.js
+++ initOneTap.js
@@ -1,25 +1,29 @@
 
-const VKID = window.VKIDSDK;
-
-VKID.Config.init({
-  app: 53969710,
-  redirectUrl: 'https://vkid-demo.vercel.app/api/vk/callback',
-  responseMode: VKID.ConfigResponseMode.Callback,
-  source: VKID.ConfigSource.LOWCODE,
-  scope: ''
-});
-
-const oneTap = new VKID.OneTap();
-
-oneTap
-  .render({
-    container: document.getElementById('vkid-container'),
-    showAlternativeLogin: true
-  })
-  .on(VKID.WidgetEvents.ERROR, console.error)
-  .on(VKID.OneTapInternalEvents.LOGIN_SUCCESS, (payload) => {
-    const { code } = payload;
-    if (code) {
-      window.location.href = `/api/vk/callback?code=${encodeURIComponent(code)}`;
-    }
+console.log('initOneTap.js стартует');
+if ('VKIDSDK' in window) {
+  console.log('VKIDSDK найден');
+  VKIDSDK.Config.init({
+    app: 53969710,
+    redirectUrl: 'https://vkid-demo.vercel.app/api/vk/callback',
+    responseMode: VKIDSDK.ConfigResponseMode.Callback,
+    source: VKIDSDK.ConfigSource.LOWCODE
   });
+  const oneTap = new VKIDSDK.OneTap();
+  console.log('OneTap создан, пытаемся отрендерить');
+  oneTap
+    .render({
+      container: document.getElementById('vkid-container'),
+      showAlternativeLogin: true
+    })
+    .on(VKIDSDK.WidgetEvents.ERROR, (e) => {
+      console.error('VK One Tap error', e);
+    })
+    .on(VKIDSDK.OneTapInternalEvents.LOGIN_SUCCESS, ({ code, device_id }) => {
+      console.log('LOGIN_SUCCESS', { code, device_id });
+      if (code && device_id) {
+        window.location.href = `/api/vk/callback?code=${encodeURIComponent(code)}&device_id=${encodeURIComponent(device_id)}`;
+      }
+    });
+} else {
+  console.error('VKIDSDK не найден!');
+}

45b7c8052d3284dcd77ab24364641cc68b2fff31
1v1ad <1v1ad@proton.me>
2025-07-29 23:48:54 +0300

    Add files via upload

---
+++ public/initOneTap.js
@@ -0,0 +1,28 @@
+
+console.log('initOneTap.js стартует');
+if ('VKIDSDK' in window) {
+  console.log('VKIDSDK найден');
+  VKIDSDK.Config.init({
+    app: 53969710,
+    redirectUrl: 'https://vkid-demo.vercel.app/api/vk/callback',
+    responseMode: VKIDSDK.ConfigResponseMode.Callback,
+    source: VKIDSDK.ConfigSource.LOWCODE
+  });
+  const oneTap = new VKIDSDK.OneTap();
+  oneTap
+    .render({
+      container: document.getElementById('vkid-container'),
+      showAlternativeLogin: true
+    })
+    .on(VKIDSDK.WidgetEvents.ERROR, (e) => {
+      console.error('VK One Tap error', e);
+    })
+    .on(VKIDSDK.OneTapInternalEvents.LOGIN_SUCCESS, ({ code, device_id }) => {
+      console.log('LOGIN_SUCCESS', { code, device_id });
+      if (code && device_id) {
+        window.location.href = `/api/vk/callback?code=${encodeURIComponent(code)}&device_id=${encodeURIComponent(device_id)}`;
+      }
+    });
+} else {
+  console.error('VKIDSDK не найден!');
+}

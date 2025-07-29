3b93e1257b7a1b48a11e0c8d4d0a1174009423d8
1v1ad <1v1ad@proton.me>
2025-07-29 15:26:00 +0300

    Add files via upload

--- initOneTap.js
+++ initOneTap.js
@@ -1,29 +1,33 @@
 
 console.log('initOneTap.js стартует');
-if ('VKIDSDK' in window) {
-  console.log('VKIDSDK найден');
-  VKIDSDK.Config.init({
-    app: 53969710,
-    redirectUrl: 'https://vkid-demo.vercel.app/api/vk/callback',
-    responseMode: VKIDSDK.ConfigResponseMode.Callback,
-    source: VKIDSDK.ConfigSource.LOWCODE
-  });
-  const oneTap = new VKIDSDK.OneTap();
-  console.log('OneTap создан, пытаемся отрендерить');
-  oneTap
-    .render({
-      container: document.getElementById('vkid-container'),
-      showAlternativeLogin: true
-    })
-    .on(VKIDSDK.WidgetEvents.ERROR, (e) => {
-      console.error('VK One Tap error', e);
-    })
-    .on(VKIDSDK.OneTapInternalEvents.LOGIN_SUCCESS, ({ code, device_id }) => {
-      console.log('LOGIN_SUCCESS', { code, device_id });
-      if (code && device_id) {
-        window.location.href = `/api/vk/callback?code=${encodeURIComponent(code)}&device_id=${encodeURIComponent(device_id)}`;
-      }
+function runVKID() {
+  if ('VKIDSDK' in window) {
+    console.log('VKIDSDK найден');
+    VKIDSDK.Config.init({
+      app: 53969710,
+      redirectUrl: 'https://vkid-demo.vercel.app/api/vk/callback',
+      responseMode: VKIDSDK.ConfigResponseMode.Callback,
+      source: VKIDSDK.ConfigSource.LOWCODE
     });
-} else {
-  console.error('VKIDSDK не найден!');
+    const oneTap = new VKIDSDK.OneTap();
+    console.log('OneTap создан, пытаемся отрендерить');
+    oneTap
+      .render({
+        container: document.getElementById('vkid-container'),
+        showAlternativeLogin: true
+      })
+      .on(VKIDSDK.WidgetEvents.ERROR, (e) => {
+        console.error('VK One Tap error', e);
+      })
+      .on(VKIDSDK.OneTapInternalEvents.LOGIN_SUCCESS, ({ code, device_id }) => {
+        console.log('LOGIN_SUCCESS', { code, device_id });
+        if (code && device_id) {
+          window.location.href = `/api/vk/callback?code=${encodeURIComponent(code)}&device_id=${encodeURIComponent(device_id)}`;
+        }
+      });
+  } else {
+    console.error('VKIDSDK не найден! Ждём...');
+    setTimeout(runVKID, 300);
+  }
 }
+runVKID();

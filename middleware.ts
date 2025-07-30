a37a1c96f1589135e597efd5962bd1ca852becae
1v1ad <1v1ad@proton.me>
2025-07-30 08:35:37 +0300

    Delete middleware.ts

--- middleware.ts
+++
@@ -1,8 +0,0 @@
-
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-
-export function middleware(request: NextRequest) {
-  console.log('>> Middleware fired for:', request.nextUrl.pathname);
-  return NextResponse.next();
-}

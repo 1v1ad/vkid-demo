1e2d0270797f7ced79292db2ee522f1d759e2e9c
1v1ad <1v1ad@proton.me>
2025-07-30 08:08:15 +0300

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,3 @@
-import React from "react";
-
 export default function Home() {
-  return (
-    <div style={{ padding: 20, fontFamily: "sans-serif" }}>
-      <h1>VK ID One Tap Demo</h1>
-      <p>Добро пожаловать. Всё работает!</p>
-    </div>
-  );
+  return <h1 style={{ padding: 40 }}>It works!</h1>;
 }

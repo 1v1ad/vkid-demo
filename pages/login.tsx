1892dbce7ec166c7d7f793018c0b71dec9e070d2
1v1ad <1v1ad@proton.me>
2025-07-30 03:16:39 +0300

    Add files via upload

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,36 +1,3 @@
-import { useEffect, useRef } from "react";
-
-declare global {
-  interface Window {
-    VKIDSDK: any;
-  }
-}
-
 export default function Login() {
-  const ref = useRef(null);
-
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://vk.com/js/api/openapi.js?169";
-    script.async = true;
-    script.onload = () => {
-      if (window.VKIDSDK && ref.current) {
-        window.VKIDSDK.Widget.init({
-          container: ref.current,
-          app: parseInt(process.env.NEXT_PUBLIC_VK_CLIENT_ID || "0"),
-          callback: function (data: any) {
-            console.log("VKID →", data);
-            window.location.href = "/lobby";
-          },
-        });
-      }
-    };
-    document.body.appendChild(script);
-  }, []);
-
-  return (
-    <div style={{ display: "flex", justifyContent: "center", marginTop: "50px" }}>
-      <div ref={ref}></div>
-    </div>
-  );
+  return <h1>Страница входа</h1>;
 }
